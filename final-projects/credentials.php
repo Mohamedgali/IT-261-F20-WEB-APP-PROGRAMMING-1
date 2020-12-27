bca4381ecda733435f4abab051a226fd195fe826
Mohamedgali <56414297+Mohamedgali@users.noreply.github.com>
2020-12-27 04:37:59 -0800

    Update credentials.php

--- final-projects/credentials.php
+++ final-projects/credentials.php
@@ -1,9 +1,1 @@
-<?php 
-
-//credentials for our database
-
-define('DB_HOST', 'mysql.mohamedgali.com');
-define('DB_USER', 'mohamedgalicom1');
-define('DB_PASSWORD', 'MqNSXG?R');
-define('DB_NAME', 'mohamedgalicom_week7');
 
